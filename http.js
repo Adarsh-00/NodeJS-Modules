dc4cd25cd1bf57c42744550fd2c44a0d49d5548d
Adarsh-00 <134307234+Adarsh-00@users.noreply.github.com>
2025-08-11 19:37:19 +0530

    Add files via upload

---
+++ http.js
@@ -0,0 +1,12 @@
+const http = require('http');  
+
+const server = http.createServer((req, res) => {
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+    res.end(JSON.stringify({
+        data: 'Hello, World!'
+    }));
+});
+
+server.listen(8080);
+
+console.log('Server is running on http://localhost:8080');
